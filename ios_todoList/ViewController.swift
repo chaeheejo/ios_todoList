058d8bf1e4a929bb1c993e14663bfb4add5a6e5e
chaeheejo <60cogml@gmail.com>
2023-02-02 18:54:58 +0900

    connect RealmSwift and set navigation controller

--- ios_todoList/ViewController.swift
+++ ios_todoList/ViewController.swift
@@ -6,14 +6,41 @@
 //
 
 import UIKit
+import RealmSwift
 
-class ViewController: UIViewController {
+class ToDoListItem: Object {
+    @objc dynamic var item: String = ""
+    @objc dynamic var date: Date = Date()
+}
+
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var table: UITableView!
+    
+    private var data = [ToDoListItem]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        table.register(UITableView.self, forCellReuseIdentifier: "cell")
+        table.delegate = self
+        table.dataSource = self
     }
-
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = data[indexPath.row].item
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
 
 }
 
