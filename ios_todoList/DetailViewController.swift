c2ef900b32d078589b36110477541267edb8f612
chaeheejo <60cogml@gmail.com>
2023-02-03 19:37:03 +0900

    center alignment in date picker

--- ios_todoList/DetailViewController.swift
+++ ios_todoList/DetailViewController.swift
@@ -36,8 +36,8 @@ class DetailViewController: UIViewController {
     @objc func didTapDelete(){
         
         let alret = UIAlertController(title: "", message: "you cannot undo this action", preferredStyle: .actionSheet)
-        let yes = UIAlertAction(title: "Cancel", style: .default, handler: nil)
-        let no = UIAlertAction(title: "Delete this item?", style: .destructive) { action in
+        let cancel = UIAlertAction(title: "Cancel", style: .default, handler: nil)
+        let delete = UIAlertAction(title: "Delete this item?", style: .destructive) { action in
             guard let deleteItem = self.item else{
                 return
             }
@@ -50,8 +50,8 @@ class DetailViewController: UIViewController {
             self.navigationController?.popToRootViewController(animated: true)
         }
 
-        alret.addAction(no)
-        alret.addAction(yes)
+        alret.addAction(delete)
+        alret.addAction(cancel)
 
         present(alret, animated: true, completion: nil)
     
--- ios_todoList/EntryViewController.swift
+++ ios_todoList/EntryViewController.swift
@@ -23,9 +23,17 @@ class EntryViewController: UIViewController, UITextFieldDelegate {
         textField.delegate = self
         
         datePicker.setDate(Date(), animated: true)
+        datePicker.contentHorizontalAlignment = .center
+        
         
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Save", style: .done, target: self, action: #selector(didTapSaveButton))
     }
+
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+           self.view.endEditing(true)
+       }
+    
     
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         textField.resignFirstResponder()
